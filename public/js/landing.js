802eeae209e4b9d5d380d6804093ad9c09b263e3
Dlovan <medes_117@live.se>
2023-06-07 15:36:53 +0200

    pricing design

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,33 +1,9 @@
-const signinModal = document.querySelector('.signinModal')
-const signinBtn = document.querySelector('.sign-in')
-const modalOverlay = document.querySelector('.modal-container')
-const mainContent = document.querySelector('.main')
-const header = document.querySelector('.headnav')
-const footer = document.querySelector('.theFoot')
-
-signinBtn.addEventListener('click', function (e) {
-    signinModal.classList.toggle('signinModal-active')
-    modalOverlay.classList.toggle('modal-overlay')
-    mainContent.classList.toggle('modalBlur')
-    header.classList.toggle('modalBlur')
-    footer.classList.toggle('modalBlur')
-})
-
-document.addEventListener('mousedown', function (e) {
-    if (!signinModal.contains(e.target) && signinModal.classList.contains('signinModal-active')) {
-        mainContent.classList.toggle('modalBlur')
-        header.classList.toggle('modalBlur')
-        footer.classList.toggle('modalBlur')
-        signinModal.classList.toggle('signinModal-active')
-        modalOverlay.classList.toggle('modal-overlay')
-    }
-})
-
-
 // Pricing button
 const monthly = document.querySelector('.monthly')
 const yearly = document.querySelector('.yearly')
 const highlighter = document.querySelector('.highlighter')
+const priceInteger = document.querySelector('.price-integer')
+const billedAnually = document.querySelector('.billed-anually')
 
 let defaultBoolean = true
 
@@ -36,6 +12,8 @@ monthly.addEventListener('click', function () {
         highlighter.classList.remove('highlighter-active-ml', 'highlighter-active-wh')
         yearly.classList.remove('yearly-active')
         monthly.classList.remove('monthly-inactive')
+        priceInteger.textContent = "$49/month"
+        billedAnually.classList.remove('billed-anually-active')
         defaultBoolean = true
     }
 })
@@ -45,6 +23,8 @@ yearly.addEventListener('click', function () {
         highlighter.classList.add('highlighter-active-ml', 'highlighter-active-wh')
         monthly.classList.add('monthly-inactive')
         yearly.classList.add('yearly-active')
+        priceInteger.textContent = "$39/month"
+        billedAnually.classList.add('billed-anually-active')
         defaultBoolean = false
     }
 })
