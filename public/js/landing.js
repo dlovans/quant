03d59d318bb66259d81df2b2e9b3a39d8a57a4f3
Dlovan <medes_117@live.se>
2023-06-10 17:51:08 +0200

    faq design and layout. error with margin of each card: fixed

--- public/js/landing.js
+++ public/js/landing.js
@@ -28,3 +28,18 @@ yearly.addEventListener('click', function () {
         defaultBoolean = false
     }
 })
+
+
+
+// Faq functionality
+
+const faqCards = document.querySelectorAll('.faq')
+
+for (let faqCard of faqCards) {
+    faqCard.addEventListener('click', function () {
+        console.log(faqCard.childNodes)
+        faqCard.childNodes[3].childNodes[0].classList.toggle('arrow-active')
+        faqCard.childNodes[1].childNodes[3].classList.toggle('q-answer-active')
+        faqCard.childNodes[1].childNodes[1].classList.toggle('q-question-active')
+    })
+}
