09b9b7d0a8196b14dd586f9aed7242fa4d283966
Dlovan <medes_117@live.se>
2023-07-13 19:17:36 +0200

    view overylay on menu open. might need to change structure for sidemenu

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -5,6 +5,7 @@ const sideNavbar = document.querySelector('.side-navbar')
 const sideMenu = document.querySelector('.sidemenu')
 const sideMenuBtns = document.querySelectorAll('.sideMenu-button')
 const sideMenuOverlay = document.querySelector('.overlay')
+const viewOverlay = document.querySelector('.viewOverlay')
 
 // This variable's related to the sidemenu item button click, not hamburger menu
 let sideMenuItemClick;
@@ -14,22 +15,26 @@ let menuOpen = false
 let first;
 let second;
 let third;
-menuBtn.addEventListener('click', function () {
+menuBtn.addEventListener('click', function (e) {
+    e.stopPropagation()
     if (!menuOpen) {
         clearTimeout(sideMenuItemClick)
         clearTimeout(second)
         clearTimeout(third)
         menuOpen = true
+        document.body.style.overflowY = "hidden"
+        viewOverlay.classList.add('viewOverlay-active')
         sideNavbar.classList.add('side-navbar-active')
         lineTwo.classList.add('line2-active-collapse')
         menuWrapper.classList.add('menu-wrapper-active-rotate')
         first = setTimeout(() => {
             lineTwo.classList.add('line2-active-expand')
         }, 400)
-
     } else {
         menuOpen = false
         clearTimeout(first)
+        document.body.style.overflowY = "scroll"
+        viewOverlay.classList.remove('viewOverlay-active')
         sideNavbar.classList.remove('side-navbar-active')
         lineTwo.classList.remove('line2-active-expand')
         second = setTimeout(() => {
@@ -42,8 +47,7 @@ menuBtn.addEventListener('click', function () {
 })
 
 
-// On click, move span overlay to clicked button, and hide sidemenu (remove the hide feature???)
-
+// On click, move span overlay to clicked button, hide sidemenu, remove hidden overflowY from body
 for (let sideMenuBtn of sideMenuBtns) {
     sideMenuBtn.addEventListener('click', function () {
         // Find the top value for the clicked element, minus sidemenu top value
@@ -54,6 +58,7 @@ for (let sideMenuBtn of sideMenuBtns) {
         // Close the sidemenu, clearTimeout in case users is rapid clicker
         menuOpen = false
         clearTimeout(first)
+        document.body.style.overflowY = "scroll"
         sideMenuItemClick = setTimeout(() => {
             sideNavbar.classList.remove('side-navbar-active')
             lineTwo.classList.remove('line2-active-expand')
@@ -66,6 +71,25 @@ for (let sideMenuBtn of sideMenuBtns) {
         }, 350)
         // Add statements for http requests for each button clicked, with value in html element
     })
-
-
 }
+
+// close sidemenu if user clicks on transparent overlay that spans the whole page
+viewOverlay.addEventListener('click', function () {
+    if (menuOpen) {
+        menuOpen = false
+        clearTimeout(first)
+        viewOverlay.classList.remove('viewOverlay-active')
+        document.body.style.overflowY = "scroll"
+        sideMenuItemClick = setTimeout(() => {
+            sideNavbar.classList.remove('side-navbar-active')
+            lineTwo.classList.remove('line2-active-expand')
+            second = setTimeout(() => {
+                menuWrapper.classList.remove('menu-wrapper-active-rotate')
+            }, 250)
+            third = setTimeout(() => {
+                lineTwo.classList.remove('line2-active-expand', 'line2-active-collapse')
+            }, 400)
+        }, 50)
+    }
+})
+
